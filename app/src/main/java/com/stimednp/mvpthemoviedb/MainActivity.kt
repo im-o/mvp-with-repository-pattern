2dc9b13a4aba95346e04908f00e9e2685ae90b7a
im-o <frizgt@gmail.com>
2020-10-28 14:52:56 +0800

    :tada: project started

---
+++ app/src/main/java/com/stimednp/mvpthemoviedb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stimednp.mvpthemoviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
