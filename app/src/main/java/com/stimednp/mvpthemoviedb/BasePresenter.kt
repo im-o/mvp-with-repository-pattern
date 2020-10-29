8b66212d1ece3b23cb009f600359947da65d22ed
im-o <frizgt@gmail.com>
2020-10-30 02:40:51 +0800

    :zap: setup base

---
+++ app/src/main/java/com/stimednp/mvpthemoviedb/BasePresenter.kt
@@ -0,0 +1,10 @@
+package com.stimednp.mvpthemoviedb
+
+/**
+ * Created by rivaldy on Oct/29/2020.
+ * Find me on my lol Github :D -> https://github.com/im-o
+ */
+
+interface BasePresenter {
+    fun start()
+}
